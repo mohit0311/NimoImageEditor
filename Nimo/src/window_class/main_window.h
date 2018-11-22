ca6e347e186ba5d3db3736e7afc9f0647ee1ac69
akku9908 <akku00009@gmail.com>
2018-11-22 20:13:15 +0530

    create a main_window.h header file

---
+++ Nimo/src/window_class/main_window.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_WINDOW_H // if this header file is not defined anywhere
+#define _MAIN_WINDOW_H // define it
+
+
+
+
+
+#endif
--- Nimo/test.cpp
+++ Nimo/test.cpp
@@ -1,10 +1,13 @@
+#include <glew.h>
 #include <glfw3.h>
 #include <iostream>
 int main() {
+
 	if (!glfwInit()) {
-		std::cout << "error" << "\n";
+		std::cout << "error in initializing the library" << "\n";
 		return -1;
 	}
 	system("PAUSE");
 	return 0;
+
 }
