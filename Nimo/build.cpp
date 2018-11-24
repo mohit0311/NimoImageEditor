bdfed6e0b7542fa9df88d06e922f9142463e385e
akku9908 <akku00009@gmail.com>
2018-11-24 14:01:28 +0530

    glsl code is added in shaders

--- Nimo/build.cpp
+++ Nimo/build.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "src/MainWindow/main_window.h"
+#include "src/cMacros/macros.h"
 int main() {
-	char windowName[] = "Hello World";
-	std::cout << "HEllo all is working"<<"\n";
-	
-	if(MainWindow::createWindow(windowName, 1000, 800));
+	char windowName[] = wName;
+	if (MainWindow::createWindow(windowName, 1000, 800)) {
+		return -1;
+	}
+	else {
+		std::cout << cStat << "\n";
+	}
 	while (!MainWindow::windowShouldClose()) {
 		glClear(GL_COLOR_BUFFER_BIT);
 
---
+++ Nimo/src/cMacros/macros.h
@@ -0,0 +1,5 @@
+#pragma once
+#define wName "Nimo"
+#define cStat "....running"
+#define cGLFWstat "....failed to initiailize glfw"
+#define cGLEWstat "....failed to initialize glew"
